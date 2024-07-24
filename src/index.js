c50c2c39cb96f0baefc2ac30c8e096ebf16f591a
GuyCodes2919 <memescraft600@gmail.com>
2024-07-23 17:44:18 -0700

    Util Codes

--- src/index.js
+++ src/index.js
@@ -9,24 +9,11 @@ dotenv.config({
 
 
 connectDB()
-
-/*
-import express from "express";
-const app = express()(async () => {
-  try {
-    mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
-    app.on("error", (error) => {
-      console.log("Error: ", error);
-      throw error;
-    });
-
-    app.listen(process.env.PORT, () => {
-      console.log("Listening on port: ", process.env.PORT);
-    });
-  } catch (error) {
-    console.error("Error: ", error);
-    throw err;
-  }
-})();
-
-*/
+.then(()=>{
+    app.listen(process.env.PORT || 8000, () => {
+        console.log("Listening on port: ", process.env.PORT);
+    })
+})
+.catch((err) => {
+    console.log("MONGO DB CONNECTION FAILED!!")
+})
